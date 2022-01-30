92f81ba443a230ee57e074b5b31e76270fbfb53b
Jeancarlo <jcode2006@gmail.com>
2022-01-29 22:33:14 -0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* **************** Imports **************** */
+import React from 'react'
+import './App.css'
+
+/* **************** Variables **************** */
+
+/* **************** Functions **************** */
+function App() {
+  return <h1>Hello World</h1>
+}
+
+/* **************** Code Execution **************** */
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+/* **************** Imports **************** */
+import React from 'react'
+
+/* **************** Variables **************** */
+
+/* **************** Functions **************** */
+const Header = ({ logo, logoName, title }) => {
+  return (
+    <header className="header">
+      <div className="container-header">
+        <div className="logo">
+          <img src={logo} alt={logoName} />
+          <h2>{title}</h2>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  logo: '',
+  logoName: 'Not found',
+  title: 'React'
+}
+
+/* **************** Code Execution **************** */
+export default Header
